e9912d03b415d986dc27f4a15cca4a05a8128891
Joseba S <jjoseba@gmail.com>
2023-12-22 13:19:48 +0100

    Project structure

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from core import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='index'),
+
+    path('login/', auth_views.LoginView.as_view(), {'redirect_authenticated_user': True}, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
+        auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+]
