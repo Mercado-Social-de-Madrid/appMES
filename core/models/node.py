8f4f3cadbb242d0894249a361bab66b00b08f8ff
Alberto Moreta <albertomoreta70@gmail.com>
2024-02-15 10:44:36 +0100

    Add takahe_server field to node model

--- core/models/node.py
+++ core/models/node.py
@@ -21,6 +21,7 @@ class Node(models.Model):
     banner_image = ProcessedImageField(null=True, blank=True, upload_to=RandomFileName('node/'),
                                        verbose_name=_('Imagen principal'),
                                        processors=[ResizeToFit(756, 512, upscale=False)], format='JPEG')
+    takahe_server = models.URLField(blank=True, null=True, verbose_name=_('Servidor de Takahe'))
 
     class Meta:
         verbose_name = _('Mercado')
