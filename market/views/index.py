ca9a457d0cd1005085f19b44642881636fd475d5
Joseba S <jjoseba@gmail.com>
2024-03-11 16:02:45 +0100

    Added latest_news card for user dashboards

--- market/views/index.py
+++ market/views/index.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView, RedirectView
 
 from market.mixins.current_market import MarketMixin
 from market.models import Account, Provider
+from news.models import News
 from offers.models import Offer
 
 
@@ -26,21 +27,36 @@ class AdminDashboard(TemplateView):
 class MarketDashboard(MarketMixin, TemplateView):
     template_name = 'dashboard/market.html'
 
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['last_news'] = News.objects.filter(node=self.node).order_by('-published_date')[:3]
+        return context
 class UserDashboard(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/user.html'
 
+    def get_account(self):
+        return Account.objects.filter(owner=self.request.user).first()
+
+    def get_template_names(self):
+        account = self.get_account()
+        if isinstance(account, Provider):
+            return 'dashboard/provider.html'
+        else:
+            return 'dashboard/consumer.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data()
-        account = Account.objects.filter(owner=self.request.user).first()
+        account = self.get_account()
         context['account'] = account
 
         if isinstance(account, Provider):
             context['num_offers'] = Offer.objects.current(provider=account).count()
+
+        context['last_news'] = News.objects.filter(node=account.node).order_by('-published_date')[:3]
 
         return context
 
 class UserAccountDetail(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         account = Account.objects.filter(owner=self.request.user).first()
-        return reverse(account.detail_url, kwargs={'market':account.node.pk, 'pk':account.pk })
+        return reverse(account.detail_url, kwargs={'market':account.node.pk, 'pk': account.pk })
