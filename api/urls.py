f559d8cd128b03debfebe0d9fb9dbb32bb5ffdab
Alberto Moreta <albertomoreta70@gmail.com>
2024-03-13 15:51:10 +0100

    Update api: Return gone for old entities endpoint. Use v2 for new app

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,7 @@ from api.views.auth import LoginView, LogoutView
 from api.views.benefits import BenefitsViewSet
 from api.views.news import NewsViewSet
 from api.views.offers import OffersViewSet
+from api.views.provider import EntitiesView
 
 router = DefaultRouter()
 router.register("devices", FCMDeviceAuthorizedViewSet)
@@ -18,9 +19,11 @@ router.register(r"nodes/(?P<node>\d+)/offers", OffersViewSet)
 router.register(r"nodes/(?P<node>\d+)/benefits", BenefitsViewSet)
 
 urlpatterns = [
-    path("v1/", include(router.urls)),
-    path("v1/login/", LoginView.as_view()),
-    path("v1/logout/", LogoutView.as_view()),
+    path("v2/", include(router.urls)),
+    path("v2/login/", LoginView.as_view()),
+    path("v2/logout/", LogoutView.as_view()),
+
+    path("v1/entities/", EntitiesView.as_view())
 ]
 
 
--- api/views/provider.py
+++ api/views/provider.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseGone
 from rest_framework import viewsets
+from rest_framework.views import APIView
 
 from api.mixins.FilterByNodeMixin import FilterByNodeMixin
 from api.serializers.provider import ProviderSerializer
@@ -8,3 +10,11 @@ from market.models import Provider
 class ProviderViewSet(FilterByNodeMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
+
+
+class EntitiesView(APIView):
+    permission_classes = []
+
+    def get(self, request, format=None):
+        return HttpResponseGone()
+
