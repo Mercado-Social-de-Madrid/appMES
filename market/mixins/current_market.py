9020845083177f09982070a70cc822a6e2dee3a1
Joseba S <jjoseba@gmail.com>
2024-03-19 13:38:59 +0100

    Updated some typos in translations

--- market/mixins/current_market.py
+++ market/mixins/current_market.py
@@ -10,6 +10,10 @@ class MarketMixin(object):
         super().setup(*args, **kwargs)
         if 'market' in self.kwargs:
             self.node = Node.objects.filter(pk=self.kwargs['market']).first()
+        elif not self.request.user.is_staff:
+            account = self.request.user.accounts_managed.first()
+            if account:
+                self.node = account.node
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
