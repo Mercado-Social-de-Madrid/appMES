60ccd42d3a03435923da0cdb47198349781568bb
Joseba S <jjoseba@gmail.com>
2024-03-21 11:17:10 +0100

    Update issue with password reset email

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from authentication import views
 from django.contrib.auth import views as auth_views
@@ -10,7 +10,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(), {'redirect_authenticated_user': True}, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('account/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('account/password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('auth:password_reset_done')), name='password_reset'),
     path('account/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('account/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('account/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
