ffd706f8ffdbf9a24e378e925bc0a0a114f9526a
julio <jberzal86@gmail.com>
2024-05-17 13:50:36 +0200

    #90 - HTML editor for Provider description

--- market/forms/provider.py
+++ market/forms/provider.py
@@ -2,12 +2,16 @@ from django import forms
 
 from market.forms.account import AccountForm, CreateAccountFormMixin
 from market.models import Provider
+from ckeditor.widgets import CKEditorWidget
 
 
 class ProviderForm(AccountForm):
 
     class Meta(AccountForm.Meta):
         model = Provider
+        widgets = {
+            'description': CKEditorWidget(attrs={'cols': 80, 'rows': 30}),
+        }
 
 class CreateProviderForm(CreateAccountFormMixin, ProviderForm):
     pass
