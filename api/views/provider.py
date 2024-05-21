5f2f83960989e9df2c5c6af2c414dfc484365743
Alberto Moreta <albertomoreta70@gmail.com>
2024-05-21 19:27:10 +0200

    #98 - Update provider filter

--- api/views/provider.py
+++ api/views/provider.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponseGone
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.views import APIView
 
 from api.mixins.FilterByNodeMixin import FilterByNodeMixin
 from api.serializers.provider import ProviderSerializer
 from market.models import Provider
+from market.views import ProviderFilter
 
 
 class ProviderViewSet(FilterByNodeMixin, viewsets.ReadOnlyModelViewSet):
     queryset = Provider.objects.all()
     serializer_class = ProviderSerializer
+    search_fields = ['name', 'description', 'short_description', 'email']
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProviderFilter
 
 
 class EntitiesView(APIView):
