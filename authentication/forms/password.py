7fd10884d8ec14b63b54aa12b5f3d9d6a9c95445
Joseba S <jjoseba@gmail.com>
2024-05-21 13:56:00 +0200

    #123 - Password reset email with custom template

--- authentication/forms/password.py
+++ authentication/forms/password.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 
-from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm
+from django.contrib.auth.forms import PasswordChangeForm, SetPasswordForm, PasswordResetForm
+from django.template import loader
+
+from helpers import mailing
 from helpers.forms.BootstrapForm import BootstrapForm
 
 
@@ -10,3 +13,24 @@ class PasswordForm(BootstrapForm, PasswordChangeForm):
 
 class FirstPasswordForm(BootstrapForm, SetPasswordForm):
     pass
+
+
+class PasswordReset(PasswordResetForm):
+
+    def send_mail(
+        self,
+        subject_template_name,
+        email_template_name,
+        context,
+        from_email,
+        to_email,
+        html_email_template_name=None,
+    ):
+        title = loader.render_to_string(subject_template_name, context)
+
+        mailing.send_template_email(
+            title=title,
+            destination=to_email,
+            template_name='password_reset',
+            template_params=context,
+        )
--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, reverse_lazy
 from authentication import views
 from django.contrib.auth import views as auth_views
 
+from authentication.forms.password import PasswordReset
+
 app_name = 'auth'
 urlpatterns = [
     path('profile/edit/', views.EditProfile.as_view(), name='edit_profile'),
@@ -10,7 +12,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(), {'redirect_authenticated_user': True}, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('account/password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('auth:password_reset_done')), name='password_reset'),
+    path('account/password_reset/', auth_views.PasswordResetView.as_view(form_class=PasswordReset, success_url=reverse_lazy('auth:password_reset_done')), name='password_reset'),
     path('account/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('account/reset/<uidb64>/<token>/', views.PasswordResetView.as_view(), name='password_reset_confirm'),
     path('account/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
