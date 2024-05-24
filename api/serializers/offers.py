14757a5707b4a8806ba093d64004167341565adb
julio <jberzal86@gmail.com>
2024-05-24 04:54:15 +0200

    include provider data in offer

--- api/serializers/offers.py
+++ api/serializers/offers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
+from api.serializers.provider_simple import ProviderSimpleSerializer
 from offers.models import Offer
 
 
 class OffersSerializer(serializers.ModelSerializer):
     banner_image = serializers.StringRelatedField(source="banner_image.name")
+    provider = ProviderSimpleSerializer(many=False, read_only=True)
 
     class Meta:
         model = Offer
---
+++ api/serializers/provider_simple.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from market.models import Provider
+
+
+class ProviderSimpleSerializer(serializers.ModelSerializer):
+    profile_image = serializers.StringRelatedField(source="profile_image.name")
+
+    class Meta:
+        model = Provider
+        fields = ['name', 'profile_image', 'webpage_link']
