0a0d7ebfe44084e353b3982599ba9cf7458cd835
Alberto Moreta <albertomoreta@etik.com>
2024-06-06 12:35:58 +0200

    HA-104: Update PUT method for consumers

--- api/serializers/consumer.py
+++ api/serializers/consumer.py
@@ -3,6 +3,13 @@ from market.models import Consumer
 
 
 class ConsumerSerializer(serializers.ModelSerializer):
+    cif = serializers.ReadOnlyField()
+    node = serializers.ReadOnlyField(source='node.pk')
+    member_id = serializers.ReadOnlyField()
+    owner = serializers.ReadOnlyField(source='owner.pk')
+    is_active = serializers.ReadOnlyField()
+    last_updated = serializers.ReadOnlyField()
+    registration_date = serializers.ReadOnlyField()
 
     class Meta:
         model = Consumer
--- api/views/consumer.py
+++ api/views/consumer.py
@@ -1,12 +1,17 @@
-from django.http import HttpResponseForbidden
+import logging
+
+from django.http import HttpResponseBadRequest
 from rest_framework import authentication, status
 from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from api.mixins.FilterByNodeMixin import FilterByNodeMixin
 from api.serializers.consumer import ConsumerSerializer
 from market.models import Consumer
+
+logger = logging.getLogger(__name__)
 
 
 class ConsumerViewSet(FilterByNodeMixin, RetrieveUpdateAPIView):
@@ -19,4 +24,13 @@ class ConsumerViewSet(FilterByNodeMixin, RetrieveUpdateAPIView):
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, *args, **kwargs):
-        return HttpResponseForbidden('{"message":"Esta funcionalidad está desactivada temporalmente"}')
+        data = JSONParser().parse(request)
+        consumer = Consumer.objects.get(owner=request.user)
+        consumer_serializer = ConsumerSerializer(consumer, data=data)
+        if consumer_serializer.is_valid():
+            consumer_serializer.save()
+            return Response(data=consumer_serializer.data)
+
+        logger.error("Error al editar perfil de consumidora:")
+        logger.error(consumer_serializer.errors)
+        return HttpResponseBadRequest('{"message":"Ha habido un error al editar el perfil"}')
