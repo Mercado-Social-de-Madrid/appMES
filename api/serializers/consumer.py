c9abedc8c39c807fa6049a30fbaa100e98a56fd4
julio <jberzal86@gmail.com>
2024-06-12 16:00:01 +0200

    hotfix - retrocompatible consumer fields

--- api/serializers/consumer.py
+++ api/serializers/consumer.py
@@ -9,6 +9,7 @@ class ConsumerSerializer(AccountSerializer):
     name = serializers.CharField(source='first_name')
     surname = serializers.CharField(source='last_name')
     nif = serializers.CharField(source='cif')
+    profile_thumbnail = serializers.CharField(source='profile_image')
 
     class Meta:
         model = Consumer
