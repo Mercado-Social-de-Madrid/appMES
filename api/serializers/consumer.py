df984ef3c24084f54ae9b86d5de326daaae3530d
julio <jberzal86@gmail.com>
2024-06-12 15:49:38 +0200

    hotfix - retrocompatible consumer fields

--- api/serializers/consumer.py
+++ api/serializers/consumer.py
@@ -1,10 +1,14 @@
-
+from rest_framework import serializers
 
 from api.serializers.account import AccountSerializer
 from market.models import Consumer
 
 
 class ConsumerSerializer(AccountSerializer):
+
+    name = serializers.CharField(source='first_name')
+    surname = serializers.CharField(source='last_name')
+    nif = serializers.CharField(source='cif')
 
     class Meta:
         model = Consumer
