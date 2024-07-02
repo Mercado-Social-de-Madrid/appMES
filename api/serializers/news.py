68491d1cfcedff416b6354d6f8746d4700d7c59f
alberto <albertomoreta@etik.com>
2024-07-02 16:52:50 +0200

    HA-209 - DRF integration

--- api/serializers/news.py
+++ api/serializers/news.py
@@ -1,9 +1,11 @@
+
 from rest_framework import serializers
 
+from api.mixins.ExcludeTranslationFieldsMixin import ExcludeTranslationFieldsMixin
 from news.models import News
 
 
-class NewsSerializer(serializers.ModelSerializer):
+class NewsSerializer(ExcludeTranslationFieldsMixin, serializers.ModelSerializer):
     banner_image = serializers.StringRelatedField(source="banner_image.name")
     id = serializers.StringRelatedField()
 
--- news/admin.py
+++ news/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
 
+from modeltranslation.admin import TabbedTranslationAdmin
 from news.models import News
 
 
-# Register your models here.
+
 @admin.register(News)
-class NewsAdmin(TranslationAdmin):
+class NewsAdmin(TabbedTranslationAdmin):
     list_display = ["title", "node", ]
     list_filter = ["node"]
