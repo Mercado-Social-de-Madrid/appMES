0a028c8f8c2d31de1fb41db2c901baeeddff9fc0
Joseba S <jjoseba@gmail.com>
2024-07-23 14:35:22 +0200

    Minor fixes in user form

--- authentication/forms/user.py
+++ authentication/forms/user.py
@@ -5,19 +5,21 @@ from django.conf import settings
 from django.contrib.auth.forms import SetPasswordForm
 
 from authentication.models import User
+from core.models import Node
 from helpers.forms.BootstrapForm import BootstrapForm
 from django.utils.translation import gettext_lazy as _
 
 class UserForm(BootstrapForm, forms.ModelForm):
+    node = forms.ModelChoiceField(queryset=Node.objects.all(), required=True, label=_('Mercado que gestiona'))
+
     class Meta:
         model = User
-        fields = ['first_name', 'node', 'last_name', 'email', 'preferred_locale', 'manages_multi', 'is_staff']
+        fields = ['first_name', 'node', 'email', 'preferred_locale', 'manages_multi', 'is_staff']
         widgets = {
             'is_staff': forms.CheckboxInput(attrs={'disabled': True}),
-            'first_name': forms.TextInput(attrs={'placeholder': 'Nombre'}),
-            'last_name': forms.TextInput(attrs={'placeholder': 'Apellidos'}),
+            'first_name': forms.TextInput(attrs={'placeholder': _('Nombre')}),
+            'preferred_locale': forms.Select(choices=settings.LANGUAGES),
         }
-
 
 class ProfileForm(BootstrapForm, forms.ModelForm):
     email = forms.EmailField(disabled=True)
