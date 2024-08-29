aaa007811cd4a48a9317f2f4e10ad2e70539d5c7
alberto <albertomoreta@etik.com>
2024-08-29 14:06:25 +0200

    Update version number

--- appmes/settings.py
+++ appmes/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
-VERSION = "2.0"
+VERSION = "2.1"
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 ADMINS = env.list("ADMINS")
