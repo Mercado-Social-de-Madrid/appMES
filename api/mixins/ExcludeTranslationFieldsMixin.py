3fa4de50038ae42181839f0e3cd3b10761a501c6
Alberto Moreta <albertomoreta@etik.com>
2024-08-29 13:04:08 +0200

    mes-227: Remove translation fields from API response using get_fields() instead of __init__ (#1)

--- api/mixins/ExcludeTranslationFieldsMixin.py
+++ api/mixins/ExcludeTranslationFieldsMixin.py
@@ -4,8 +4,9 @@ from modeltranslation.fields import TranslationField
 class ExcludeTranslationFieldsMixin:
     # Exclude the specific language fields from the serializer output
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
+    def get_fields(self):
+        fields = super().get_fields()
         for f in self.Meta.model._meta.fields:
-            if f.name in self.fields and isinstance(f, TranslationField):
-                self.fields.pop(f.name)
+            if f.name in fields and isinstance(f, TranslationField):
+                del fields[f.name]
+        return fields
